f173909f02a639ae0f81a9d73c0b9366ce736859
pacer1 <sauravvirat7156508@gmail.com>
2022-06-01 23:26:09 +0530

    swapping without using third variable

--- swap_number.cpp
+++ swap_number.cpp
@@ -3,9 +3,12 @@ using namespace std;
 int main(){
     int a,b,temp;
     cin>>a>>b;
-    temp=a;
-    a=b;
-    b=temp;
+    // temp=a;
+    // a=b;
+    // b=temp;
+    a+=b;
+    b=a-b;
+    a=a-b;
     cout<<a<<" "<<b;
     return 0;
 }
