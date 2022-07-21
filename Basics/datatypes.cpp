906b36e1aba721b9a2ef755230995c989cce94f6
pacer1 <sauravvirat7156508@gmail.com>
2022-07-21 23:25:21 +0530

    basic of datatypes and sizeof

---
+++ Basics/datatypes.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age=39;
+    string name="Virat";
+    char gender='M';
+    bool isMarried=true;
+    float weight=68.5;
+    cout<<name<<endl;
+    cout<<age<<endl;
+    cout<<gender<<endl;
+    cout<<weight<<endl;
+    cout<<isMarried<<"\n";
+    //primitive datatypes
+    short s=24;
+    int i;
+    long l;
+    long long ll;
+    unsigned short us;
+    unsigned int ui;
+    unsigned long ul;
+    unsigned long long ull;
+    char ch;
+    unsigned char uch;
+    float f;
+    double d;
+    long double ld;
+    bool b;
+    // void ;
+    cout<<"size of int :-"<<sizeof(s)<<endl;
+cout<<"size of int :-"<<sizeof(i)<<endl;
+cout<<"size of int :-"<<sizeof(l)<<endl;
+cout<<"size of int :-"<<sizeof(ll)<<endl;
+cout<<"size of int :-"<<sizeof(us)<<endl;
+cout<<"size of int :-"<<sizeof(ui)<<endl;
+cout<<"size of int :-"<<sizeof(ul)<<endl;
+cout<<"size of int :-"<<sizeof(ull)<<endl;
+cout<<"size of int :-"<<sizeof(ch)<<endl;
+cout<<"size of int :-"<<sizeof(uch)<<endl;
+cout<<"size of int :-"<<sizeof(f)<<endl;
+cout<<"size of int :-"<<sizeof(d)<<endl;
+cout<<"size of int :-"<<sizeof(ld)<<endl;
+cout<<"size of int :-"<<sizeof(b)<<endl;
+
+    return 0;
+}
